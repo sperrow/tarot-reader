a378c11851616d72ed6176e418721ea67af3f4fb
Matthew Sperry <mattjsperry@gmail.com>
2024-03-06 21:11:05 -0800

    font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '@fontsource-variable/playfair-display';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
