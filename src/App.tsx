501cc81a95a6feeedf9ef30ed0201e1ee7664ab5
Matthew Sperry <mattjsperry@gmail.com>
2024-03-08 18:25:59 -0800

    add github link

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import pentacle from './assets/pentacle.svg';
 import './App.css';
+import github from './assets/github-mark.svg';
 import TarotController from './TarotController.tsx';
 
 function App() {
     return (
         <>
             <header className="flex flex-col items-center justify-center">
-                <div>
+                <div className="text-4xl mb-8">
                     <h1>Tarot Reading</h1>
                 </div>
-                <img src={pentacle} className="logo" alt="pentacle" />
             </header>
-            <main>
+            <main className="pb-16">
                 <TarotController></TarotController>
             </main>
+            <footer className="absolute bottom-0 left-0 w-full pb-8 flex justify-center align-end">
+                <a href="https://github.com/sperrow/tarot-reader" target="_blank">
+                    <img src={github} alt="github" className="w-5" />
+                </a>
+            </footer>
         </>
     );
 }
