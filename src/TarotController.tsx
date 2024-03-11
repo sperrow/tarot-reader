2a56fddfe156ed47c1027dac0d5aeaa9f07f6974
Matthew Sperry <mattjsperry@gmail.com>
2024-03-11 02:27:35 -0700

    text edit

--- src/TarotController.tsx
+++ src/TarotController.tsx
@@ -109,7 +109,7 @@ function TarotController() {
                             className="font-sans w-full px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
                             onClick={() => setStep(step + 1)}
                         >
-                            Advice
+                            Ask A Question
                         </button>
                     </li>
                     <li className="mb-4">
